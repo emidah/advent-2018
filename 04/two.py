82f58411fd624fa165154abb3458367e69aac27a
Emil Dahlberg <emil.m.dahlberg@gmail.com>
2019-01-14 01:56:43 +0200

    added solution to 04 p2

---
+++ 04/two.py
@@ -0,0 +1,46 @@
+log = open('input.txt','r').readlines()
+log.sort()
+guards = { }
+
+guardString = ""
+fellAsleepAt = 0
+minute = 0
+
+minlist = [ [] for i in range(60)]
+
+for line in log:
+    if '#' in line: 
+        fellAsleepAt = 0
+        guardString = line[line.find('#')+1:]
+        guardString = guardString[:guardString.find(' ')]
+        if guardString not in guards.keys(): guards[guardString] = 0
+    elif 'falls asleep' in line:
+        fellAsleepAt = int(line[line.find(':')+1 : line.find(']')])
+        #print(guardString + " fell asleep at " + str(fellAsleepAt))
+    elif 'wakes up' in line:
+        wokeAt = int(line[line.find(':')+1 : line.find(']')])
+        for i in range(fellAsleepAt,wokeAt):
+            minlist[i].append(guardString)
+        timeAsleep =  wokeAt - fellAsleepAt
+        guards[guardString] += timeAsleep
+        #print(guardString + " woke up after " + str(timeAsleep) + " for a total of " + str(guards[guardString]))
+
+mincounts = []
+minguards = []
+for minute in minlist:
+    mostSleeping = ""
+    count = 0
+    for value in set(minute):
+        newcount = minute.count(value)
+        if newcount > count:
+            count = newcount
+            mostSleeping = value
+    #print("Min " + str(minlist.index(minute)) + ", guard #" + mostSleeping + ", count " + str(count) )
+    mincounts.append(count)
+    minguards.append(mostSleeping)
+
+bestmin = mincounts.index( max(mincounts) )
+result = bestmin * int(minguards[bestmin])
+
+print("Final answer: in " + str(bestmin) + ", guard #" + minguards[bestmin] + ", result " + str(result) )
+    
